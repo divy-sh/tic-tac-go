da52993566f6927c0b2a2371aee39ccaba0af84d
divyendu <divyendua@gmail.com>
2024-08-18 01:25:34 -0400

    add UI tests

---
+++ view/options_test.go
@@ -0,0 +1,44 @@
+package view
+
+import (
+	"fmt"
+	"testing"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/test"
+	"github.com/divy-sh/tic-tac-go/controller"
+)
+
+func TestNewOptions(t *testing.T) {
+	app := app.New()
+	defer app.Quit()
+	control = controller.NewController()
+	gameStatus = control.GetGameStatus()
+	options := NewOptions()
+	if options == nil {
+		t.Errorf("expected NewOptions to be not nil, got nil")
+	}
+	if statusText.Text != gameStatus.GameStatus {
+		t.Errorf("expected status text to be %s, got %s", statusText.Text, gameStatus.GameStatus)
+	}
+}
+
+func TestRestart(t *testing.T) {
+	app := app.New()
+	defer app.Quit()
+	control = controller.NewController()
+	gameStatus = control.GetGameStatus()
+	options := NewOptions()
+	if options == nil {
+		t.Errorf("expected NewOptions to be not nil, got nil")
+	}
+	gameStatus, _ = control.Move(1, 1)
+	if gameStatus.Board[1][1] != 1 {
+		t.Errorf("expected board at 1, 1 to be %d, got %d", 1, gameStatus.Board[1][1])
+	}
+	test.Tap(restartButton)
+	if gameStatus.Board[1][1] != 0 {
+		t.Errorf("expected board at 1, 1 to be %d, got %d", 0, gameStatus.Board[1][1])
+	}
+	fmt.Println(gameStatus)
+}
